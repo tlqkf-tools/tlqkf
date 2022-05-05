bd876a0ecfa132c24ffe3b289e47b5425f9dfe85
Juunini (쥬니니) <juuni.ni.i@gmail.com>
2022-05-06 01:51:15 +0900

    Colorize in help message

--- src/constants/en_help.rs
+++ src/constants/en_help.rs
@@ -1,27 +1,47 @@
-pub const ENGLISH_HELP_MESSAGE: &str = "
-usage: tlqkf [-v] [-h] [-r] [-y]
+extern crate colored;
+use crate::constants::en_help::colored::Colorize;
+
+pub fn english_help_message() -> String {
+  return format!(
+    "
+{usage}: {tlqkf} [-v] [-h] [-r] [-y]
              [--version] [--help] [--repeat] [--yes]
              [qjwjs] [ehdna] [..] [;;] [rkwmdk!]
 
-options:
-  -v, --version
+{options}:
+  {version}
                         show program's version and exit
-  -h, --help
+  {help}
                         show this help message and exit
-  -r, --repeat
+  {repeat}
                         repeat on failure
-  -y, --yes
+  {yes}
                         execute fixed command without confirmation
 
-sub commands:
-  qjwjs
+{subcommands}:
+  {qjwjs}
                         show program's version(한국어) and exit
-  help
+  {help_subcommand}
                         show this help message and exit
-  ehdna
+  {ehdna}
                         show this help message(한국어) and exit
-  ., .., ...
+  {repeat_subcommand}
                         repeat on failure
-  ;, ;;, ;;;, rkwmdk, rkwmdk!
+  {rkwmdk}
                         execute fixed command without confirmation
-";
+",
+    usage = "usage".green(),
+    options = "options".green(),
+    subcommands = "sub commands".green(),
+    tlqkf = "tlqkf".yellow(),
+    version = "-v, --version".cyan(),
+    help = "-h, --help".cyan(),
+    repeat = "-r, --repeat".cyan(),
+    yes = "-y, --yes".cyan(),
+    qjwjs = "qjwjs".cyan(),
+    help_subcommand = "help".cyan(),
+    ehdna = "ehdna".cyan(),
+    repeat_subcommand = "., .., ...".cyan(),
+    rkwmdk = ";, ;;, ;;;, rkwmdk, rkwmdk!".cyan(),
+  );
+}
--- src/constants/ko_help.rs
+++ src/constants/ko_help.rs
@@ -1,13 +1,27 @@
-pub const KOREAN_HELP_MESSAGE: &str = "
-사용법: 시발 [qjwjs] [ehdna] [..] [;;] [rkwmdk!]
+extern crate colored;
+use crate::constants::ko_help::colored::Colorize;
 
-커맨드:
-  qjwjs (버전)
-                        시발 버전을 한국어로 출력합니다
-  ehdna (도움)
-                        시발 사용법을 한국어로 출력합니다
-  ., .., ...
-                        시발 취소하기 전 까지 반복적으로 실행됩니다
-  ;, ;;, ;;;, rkwmdk, rkwmdk! (가즈아!)
-                        방금 실패한 커멘드를 물어보지 않고 곧장 시발합니다
-";
+pub fn korean_help_message() -> String {
+  return format!(
+    "
+{usage}: {tlqkf} [qjwjs] [ehdna] [..] [;;] [rkwmdk!]
+
+{subcommand}:
+  {version}
+                        {tlqkf} 버전을 한국어로 출력합니다
+  {help}
+                        {tlqkf} 사용법을 한국어로 출력합니다
+  {repeat}
+                        {tlqkf} 취소하기 전 까지 반복적으로 실행됩니다
+  {yes}
+                        방금 실패한 커멘드를 물어보지 않고 곧장 {tlqkf}합니다
+",
+    usage = "사용법".green(),
+    subcommand = "커맨드".green(),
+    tlqkf = "시발".yellow(),
+    version = "qjwjs (버전)".cyan(),
+    help = "ehdna (도움)".cyan(),
+    repeat = "., .., ...".cyan(),
+    yes = ";, ;;, ;;;, rkwmdk, rkwmdk! (가즈아!)".cyan(),
+  );
+}
